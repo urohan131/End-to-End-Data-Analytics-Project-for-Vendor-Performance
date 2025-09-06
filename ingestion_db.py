fc5d5f99c2b48689d87bea9d753e5c6baeee9c8f
Rohan Kumar <113224260+urohan131@users.noreply.github.com>
2025-09-07 00:24:11 +0530

    Add files via upload

---
+++ ingestion_db.py
@@ -0,0 +1,35 @@
+import pandas as pd 
+import os
+from sqlalchemy import create_engine
+import logging
+import time
+
+logging.basicConfig(
+    filename = "logs/ingestion_db.log",
+    level = logging.DEBUG,
+    format = "%(asctime)s-%(levelname)s-%(message)s",
+    filemode ="a"
+)
+
+engine = create_engine('sqlite:///inventory.db')
+
+def ingest_db(df, table_name,engine):
+    '''this function will ingest the dataframses into database table'''
+    df.to_sql(table_name, con= engine, if_exists = 'replace', index = False,chunksize=10000)
+
+def load_raw_data():
+    '''this function will load the CSVs as dataframes and ingest into db'''
+    start = time.time()
+    for file in os.listdir('data'):
+        if '.csv' in file:
+            df = pd.read_csv('data/'+file)
+            logging.info(f'Ingesting {file} in db')
+            ingest_db(df, file[:-4],engine0
+
+    end = time.time()
+    total_time = (end-start)/60
+    logging.info('-------------------------Ingestion Complete-----------------------')
+    logging.info(f'\nTotal Time Take: {total_time} minutes')
+
+if __name__ == '__main__':
+    load_raw_data()
